ca677c424cf01ac4c3d2f6d21168dec5bee94f74
hemanth04/09 <hemanthbalakrishnan2002@gmail.com>
2023-04-11 16:14:01 +0530

    final psd

--- assets/js/index.js
+++ assets/js/index.js
@@ -32,3 +32,9 @@ $(document).ready(function() {
         },
     });
 });
+$(function() {
+    $(document).scroll(function() {
+        var $nav = $(".navbar-fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
